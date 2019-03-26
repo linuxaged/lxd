f503c1c65dc3d10ce358f78de0653712db1adf83
马迪 <madi1@100tal.com>
2019-03-26 21:49:58 +0800

    impl: SmallVector

---
+++ smallvector.cpp
@@ -0,0 +1,6 @@
+#include "smallvector.h"
+
+SmallVector::SmallVector()
+{
+
+}
---
+++ smallvector.h
@@ -0,0 +1,11 @@
+#ifndef SMALLVECTOR_H
+#define SMALLVECTOR_H
+
+
+class SmallVector
+{
+public:
+    SmallVector();
+};
+
+#endif // SMALLVECTOR_H
