8788f3f8bfd8831681f6e5956cda7d82157968d9
tracyma <1991md@gmail.com>
2020-04-24 20:42:22 +0800

    API: remove needless const

--- src/encoding.cpp
+++ src/encoding.cpp
@@ -3,7 +3,7 @@
 #include <stringapiset.h>
 
 namespace lxd {
-	std::string utf8_encode(const std::wstring_view wstr) {
+	std::string utf8_encode(std::wstring_view wstr) {
 		if(wstr.empty()) return {};
 		int size_needed = WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), NULL, 0, NULL, NULL);
 		std::string strTo(size_needed, 0);
@@ -11,7 +11,7 @@ namespace lxd {
 		return strTo;
 	}
 
-	std::wstring utf8_decode(const std::string_view str) {
+	std::wstring utf8_decode(std::string_view str) {
 		if(str.empty()) return {};
 		int size_needed = MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), NULL, 0);
 		std::wstring wstrTo(size_needed, 0);
--- src/encoding.h
+++ src/encoding.h
@@ -5,7 +5,7 @@
 
 namespace lxd {
 	// Convert a wide Unicode string to an UTF8 string
-	DLL_PUBLIC std::string utf8_encode(const std::wstring_view wstr);
+	DLL_PUBLIC std::string utf8_encode(std::wstring_view wstr);
 	// Convert an UTF8 string to a wide Unicode String
-	DLL_PUBLIC std::wstring utf8_decode(const std::string_view str);
+	DLL_PUBLIC std::wstring utf8_decode(std::string_view str);
 }
